9f357ba8c07a6193020b20f20b943b1ed4e8005f
Alexey Cemuh <a.cemuh@voskhod.ru>
2017-06-30 18:36:58 +0300

    add tool bar on panels

--- src/ControlLeftPanel.cpp
+++ src/ControlLeftPanel.cpp
@@ -2,11 +2,19 @@
 #include "ControlLeftPanel.h"
 #include "ui_ControlLeftPanel.h"
 
+// Qt
+#include <QToolBar>
+#include <QPushButton>
+
 ControlLeftPanel::ControlLeftPanel(QWidget *parent)
   : QMainWindow(parent)
   , ui(new Ui::ControlLeftPanel)
 {
   ui->setupUi(this);
+
+  QToolBar *toolBar = new QToolBar(this);
+  toolBar->setMovable(false);
+  addToolBar(Qt::TopToolBarArea, toolBar);
 
   setWindowTitle("Панель управления");
 }
--- src/ControlRightPanel.cpp
+++ src/ControlRightPanel.cpp
@@ -2,11 +2,21 @@
 #include "ControlRightPanel.h"
 #include "ui_ControlRightPanel.h"
 
+// Qt
+#include <QToolBar>
+#include <QPushButton>
+
 ControlRightPanel::ControlRightPanel(QWidget *parent)
   : QMainWindow(parent)
   , ui(new Ui::ControlRightPanel)
 {
   ui->setupUi(this);
+
+  QToolBar *toolBar = new QToolBar(this);
+  toolBar->setMovable(false);
+  addToolBar(Qt::TopToolBarArea, toolBar);
+
+  setWindowState(Qt::WindowFullScreen);
 
   setWindowTitle("Панель управления");
 }
