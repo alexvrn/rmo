1720de40a4ee5bed2dec314de0376df886110cb4
alexvrn <alexcemuh@mail.ru>
2017-07-02 17:30:13 +0300

    add date time

--- src/RightPanel.cpp
+++ src/RightPanel.cpp
@@ -1,14 +1,30 @@
+// Local
 #include "RightPanel.h"
 #include "ui_RightPanel.h"
 
-RightPanel::RightPanel(QWidget *parent) :
-    QFrame(parent),
-    ui(new Ui::RightPanel)
+// Qt
+#include <QTimer>
+#include <QDateTime>
+
+RightPanel::RightPanel(QWidget *parent)
+  : QFrame(parent)
+  , ui(new Ui::RightPanel)
+  , m_timer(new QTimer(this))
 {
-    ui->setupUi(this);
+  ui->setupUi(this);
+  connect(m_timer, SIGNAL(timeout()), SLOT(timer()));
+  m_timer->start(1000);
 }
+
 
 RightPanel::~RightPanel()
 {
-    delete ui;
+  delete ui;
+}
+
+
+void RightPanel::timer()
+{
+  const QDateTime currentDateTime = QDateTime::currentDateTime();
+  ui->dateTimeLabel->setText(currentDateTime.toString("dd MMMM hh:mm:ss"));
 }
--- src/RightPanel.h
+++ src/RightPanel.h
@@ -1,22 +1,30 @@
 #ifndef RIGHTPANEL_H
 #define RIGHTPANEL_H
 
+// Qt
 #include <QFrame>
+class QTimer;
 
+// UI
 namespace Ui {
-class RightPanel;
+  class RightPanel;
 }
 
 class RightPanel : public QFrame
 {
-    Q_OBJECT
+  Q_OBJECT
 
-public:
+  public:
     explicit RightPanel(QWidget *parent = 0);
     ~RightPanel();
 
-private:
+  private slots:
+    void timer();
+
+  private:
     Ui::RightPanel *ui;
+
+    QTimer *m_timer;
 };
 
 #endif // RIGHTPANEL_H
