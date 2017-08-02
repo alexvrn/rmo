9ae915f1c41a438890755b02758f170c0ab751df
alexvrn <alexcemuh@mail.ru>
2017-08-03 00:25:02 +0300

    Правка интерфейса

--- src/Indicators/ShPIndicator.cpp
+++ src/Indicators/ShPIndicator.cpp
@@ -1,12 +1,16 @@
 // Local
 #include "ShPIndicator.h"
 #include "ui_ShPIndicator.h"
+#include "ShPIndicatorItem.h"
 
 ShPIndicator::ShPIndicator(QWidget *parent)
   : QWidget(parent)
   , ui(new Ui::ShPIndicator)
 {
   ui->setupUi(this);
+
+  ui->indicatorsHorizontalLayout->addWidget(new ShPIndicatorItem(this));
+  ui->indicatorsHorizontalLayout->addWidget(new ShPIndicatorItem(this));
 }
 
 
