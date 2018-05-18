f4d55f9fa5525169eb0d2aa84b3859eb1332e105
Alexey Cemuh <a.cemuh@voskhod.ru>
2018-05-18 21:37:00 +0300

    Рисование цели

--- src/CPColorMap/CPColorMap.cpp
+++ src/CPColorMap/CPColorMap.cpp
@@ -20,8 +20,13 @@ void CPColorMap::draw(QCPPainter *painter)
 {
   QCPColorMap::draw(painter);
 
-  painter->setPen(Qt::red);
+  painter->setPen(Qt::white);
 
   const QPointF point = coordsToPixels(m_point.x(), m_point.y());
-  painter->drawRect(point.x()-5, point.y()-5, 5, 5);
+
+  QPainterPath path;
+  path.addEllipse(point, 5, 5);
+  path.addPolygon(QPolygonF(QVector<QPointF>() << QPointF(point.x(), point.y() - 15) << QPointF(point.x(), point.y() + 15)));
+  path.addPolygon(QPolygonF(QVector<QPointF>() << QPointF(point.x() - 15, point.y()) << QPointF(point.x() + 15, point.y())));
+  painter->drawPath(path);
 }
